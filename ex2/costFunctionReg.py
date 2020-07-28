c52e26ae065287f9d1291e313ae1fb4461890431
Len-Jon <b19031707@njupt.edu.cn>
2020-07-28 16:43:37 +0800

    完成ex2

---
+++ ex2/costFunctionReg.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+:Time:  2020/7/28 14:41
+:Author:  lenjon
+"""
+import numpy as np
+
+from ex2.costFunction import costFunction
+from ex2.sigmoid import sigmoid
+
+
+def costFunctionReg(theta, X, y, _lambda):
+    theta = theta.reshape(1, -1)
+    m = X.shape[0]
+    J = 0
+    grad = np.zeros(theta.shape)
+    pass
+    J, grad = costFunction(theta, X, y)
+
+    J_reg = (_lambda / (2 * m)) * np.sum(np.power(theta[:, 1:theta.shape[1]], 2))
+    grad_reg = _lambda / m * theta
+    grad_reg[0, 0] = 0
+
+    J += J_reg
+    grad += grad_reg
+    return J, grad
--- ex2/plotDecisionBoundary.py
+++ ex2/plotDecisionBoundary.py
@@ -7,7 +7,9 @@
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
+from ex2.mapFeature import mapFeature
 from ex2.plotData import plotData
 
 
@@ -18,9 +20,19 @@ def plotDecisionBoundary(theta, X, y):
     :param y:
     :return: ax
     """
+    theta = theta.reshape(1, -1)
     ax = plotData(X[:, 1:3], y)
-    plotting_x = np.linspace(30, 100, 100)
-    plotting_y = (-theta[0] - theta[1] * plotting_x) / theta[2]
-    ax.plot(plotting_x, plotting_y, 'y', label='Prediction', color='b')
 
+    if theta.shape[1] <= 3:
+        plotting_x = np.linspace(30, 100, 100)
+        plotting_y = (-theta[0, 0] - theta[0, 1] * plotting_x) / theta[0, 2]
+        ax.plot(plotting_x, plotting_y, 'y', label='Prediction', color='#18bfb5')
+    else:
+        u = np.linspace(-1, 1.5, 50)  # 想让图像更精确就把50改成大些（x
+        v = np.linspace(-1, 1.5, 50)
+        z = np.zeros((len(u), len(v)))
+        for i in range(len(u)):
+            for j in range(len(v)):
+                z[i, j] = mapFeature(u[i], v[j]) @ theta.T
+        ax.contour(u, v, z, 0, colors='#18bfb5')
     return ax
